ac6d485963f0d24975e95c730ca8914fd09a05cc
riicdv <155243524+riicdv@users.noreply.github.com>
2024-07-07 17:59:47 -0300

    Add files via upload

---
+++ php/config.php
@@ -0,0 +1,14 @@
+<?php
+    $dbHost = "51.81.166.66:";
+    $dbUser = "u2434908_x839dt5t2y";
+    $dbPass = "s=aqofRJC!LhMQc+j3eb7kcK";
+    $dbName = "s2434908_iread";
+
+    $conexao = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
+    
+    /*if($conexao ->connect_error) {
+        echo "Erro";
+    }else {
+        echo "Conectado com sucesso!";
+    }*/
+?>
